bec2a2fd0c22bad5c64f6dcc0f244a4898e6297b
pleasantyomi <pleasantyomi@gmail.com>
2025-04-18 16:06:59 +0100

    Update page.tsx

--- app/(app)/products/[id]/page.tsx
+++ app/(app)/products/[id]/page.tsx
@@ -1,12 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import Container from "@/components/productDetails/container";
+import { type Metadata, type ResolvingMetadata } from "next";
+import type { PageProps } from "next";
 
-interface ProductDetailsProps {
-  params: {
-    id: string;
-  };
-}
+// interface ProductDetailsProps {
+//   params: {
+//     id: string;
+//   };
+// }
 
-export default function ProductDetails({ params }: ProductDetailsProps) {
+export default function ProductDetails({ params }: PageProps) {
   return (
     <div>
       <Container params={params} />
